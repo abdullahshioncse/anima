ceba256a530b83248288f755bab2b796bc287ed1
abdullahshioncse <abdullahshayan.cse@gmail.com>
2025-12-06 00:35:55 +0600

    Add helper functions for Arabic text formatting

---
+++ system/helper/anima.php
@@ -0,0 +1,15 @@
+<?php
+// Anima Helper Functions
+
+function formatArabicText($text) {
+    // Code to format Arabic text
+}
+
+function formatPrice($price) {
+    // Code to format price in RTL
+}
+
+function addProductBadge($badge) {
+    // Code to add product badges
+}
+?>
